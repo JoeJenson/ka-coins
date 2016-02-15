c08965bfe4c99949050b996078471a24fec620da
JoeJenson <joe.j.jenson+github@gmail.com>
2016-02-15 13:32:54 -0500

    Add anonymous function to src.js

--- src.js
+++ src.js
@@ -4,3 +4,11 @@
  * @version 0.0.1
  * @license MIT
  */
+
+/*
+A closure is used here so that not all the variables declared are added to `window`.
+However, `window` is passed in as `context` so that it can be used for desired global properties.
+*/
+(function(context) {
+  
+})(window);
