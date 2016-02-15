a566732ed891fe6fca701710c39e5a0c79a143fd
JoeJenson <joe.j.jenson+github@gmail.com>
2016-02-15 13:51:41 -0500

    Add getCoins() and setCoins() locally to src.js

--- src.js
+++ src.js
@@ -11,4 +11,26 @@ However, `window` is passed in as `context` so that it can be used for desired g
 */
 (function(context) {
   
+  var storage = context.localStorage; // Get the storage object to be used from `window`
+  
+  var json = context.JSON; // Also, store the JSON object from `window`
+  
+  var math = context.Math; // ... See last comment
+  
+  var COIN_KEY = "kac-001"; // The key used in `localStorage` to store the coins is specified here as a constant
+  
+  var getCoins = function() {
+    return json.parse(storage.getItem(COIN_KEY)).coins; // Convert the string from storage, and return its `coins` property
+  };
+  
+  var setCoins = function(coinNumber) {
+    coinNumber = Math.max(coinNumber, 0); // Make sure `coinNumber` is at least zero
+    
+    storage.setItem(COIN_KEY, json.stringify({
+      coins: coinNumber
+    })); // Change the value of the stored string so that it now is an object with the correct number of coins.
+    
+    return true; // Return true to confirm that the coin-setting operation was successful
+  };
+  
 })(window);
